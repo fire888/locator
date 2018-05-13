3bf75b8cdd8f8eb2734f9604b0d05a72f8fab0cf
vasilii <porokh.vs@gmail.com>
2018-05-13 14:45:27 +0300

    test human

--- files/Air.js
+++ files/Air.js
@@ -11,7 +11,7 @@ class Air {
     this.isRemovable = false	
     this.mesh = new THREE.Mesh(
       s.geomAir,
-	  new THREE.MeshPhongMaterial( { color: 0x00aa00 } )
+      new THREE.MeshPhongMaterial( { color: 0x00aa00 } )
     )
     this.mesh.position.set( this.targetPositionDrop.x, 300, this.targetPositionDrop.z-1500 )
     s.scene.add( this.mesh )
@@ -24,29 +24,29 @@ class Air {
     this.mesh.position.z += 5
 	
     if ( this.mesh.position.z > this.targetPositionDrop.z + 3000 ) { 
-	  this.remove()
-	  return
-	}
+      this.remove()
+      return
+    }
 
     if ( this.car ) {
-	  if ( this.mesh.position.z < this.targetPositionDrop.z - 500 ) {
-	    this.carFlyWithAir()
-	  } else {
-	    s.startCarDrop( this.car )
+      if ( this.mesh.position.z < this.targetPositionDrop.z - 500 ) {
+        this.carFlyWithAir()
+      } else {
+        s.startCarDrop( this.car )
         this.car = null		
-	  }		  
-	}    
+      }		  
+    }    
   }
   
   carFlyWithAir() {
 	  
-	    this.car.model.position.set( this.mesh.position.x, this.mesh.position.y - 20, this.mesh.position.z )  			
+    this.car.model.position.set( this.mesh.position.x, this.mesh.position.y - 20, this.mesh.position.z )  			
   }
   
   remove() {
 	  
     s.scene.remove( this.mesh )
-	this.isRemovable = true	
+    this.isRemovable = true	
   }
 }
 
--- files/Parashute.js
+++ files/Parashute.js
@@ -11,12 +11,12 @@ class Parashute {
 	this.objGeom = prepearGeometryToAnimate( s.geomParashute )	
     this.mesh = new THREE.Mesh(
       this.objGeom.geom,
-	  new THREE.MeshPhongMaterial( { 
-	    color: 0x00aa00, 
-		side: THREE.DoubleSide,
-	    transparent: true,
+      new THREE.MeshPhongMaterial({ 
+        color: 0x00aa00, 
+        side: THREE.DoubleSide,
+        transparent: true,
         opacity: 1.0			
-	  } )
+	  })
     )
     this.mesh.position.set( 0, 5, 0 )
     this.car.model.add( this.mesh )
@@ -24,22 +24,22 @@ class Parashute {
   
   render() {
 	  
-	 if ( this.mesh.material.opacity < 0 ) {
-		this.remove()
-		return
-	 }	 
-	 animationClose( this.objGeom )
-	 this.mesh.material.opacity -= 0.02
+    if ( this.mesh.material.opacity < 0 ) {
+      this.remove()
+      return
+    }	 
+    animationClose( this.objGeom )
+    this.mesh.material.opacity -= 0.02
   }
   
   remove() {
 	  
-	this.car.model.remove( this.mesh )
-	this.objGeom.geom = null
-	this.objGeom = null
+    this.car.model.remove( this.mesh )
+    this.objGeom.geom = null
+    this.objGeom = null
 	
-	this.car.parashute = null
-	this.car = null
+    this.car.parashute = null
+    this.car = null
   }
 }
 
